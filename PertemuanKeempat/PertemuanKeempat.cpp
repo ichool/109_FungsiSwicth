ab14049dabebfefd4cc314c9edee84d1f74d583a
ichool <faisalaja2653@gmail.com>
2024-03-25 10:07:59 +0700

    membuat input

--- PertemuanKeempat/PertemuanKeempat.cpp
+++ PertemuanKeempat/PertemuanKeempat.cpp
@@ -2,6 +2,18 @@
 #include <iostream>
 using namespace std;
 
+int a, b;
+
+void input() {
+    cout << "bilangan pertama =";
+    cin >> a;
+
+    cout << "bilangan kedua =";
+    cin >> b;
+
+}
+
+
 
 int main()
 {
